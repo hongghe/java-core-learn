48c855bb58e7bca15a94e6c168e1122ed73778b2
haobin <815754194@qq.com>
2017-11-09 16:53:48 +0800

    静态加载顺序

--- src/main/java/org/javacore/innerclass/LoadTurn.java
+++ src/main/java/org/javacore/innerclass/LoadTurn.java
@@ -3,8 +3,11 @@ package org.javacore.innerclass;
 
 import org.javacore.rtti.ClassInitialization;
 
+import java.util.Random;
+
 /**
  * 各种静态变量加载顺序
+ *
  * @Author: HaoBin
  * @Date 2017/11/3 16:41
  */
@@ -16,7 +19,7 @@ class Initable {
 
     static final int staticFinal = 10;
 
-    static final int staticFianl2 = ClassInitialization.random.nextInt(1000);
+    static final int staticFinal2 = ClassInitialization.random.nextInt(1000);
 
     static {
         System.out.println("Initialing Initable");
@@ -45,6 +48,17 @@ class Initable3 {
 
 
 public class LoadTurn {
+    public static Random rand = new Random(47);
 
-
+    public static void main(String[] args) throws ClassNotFoundException {
+        Class initable = Initable.class;
+        System.out.println("After creating Initable ref");
+        System.out.println(Initable.staticFinal);
+        System.out.println(Initable.staticFinal2);
+        System.out.println(Initable2.staticNonFinal);
+        Class initable3 = Class.forName("org.javacore.innerclass.Initable3");
+        System.out.println("After creating Initable3 ref");
+        System.out.println(Initable3.staticNonFinal);
+        System.out.println(Initable2.a);
+    }
 }

3c82f9166026c8f628fcd7eaa66aa1ee89f4bd7d
haobin <815754194@qq.com>
2017-11-20 10:17:42 +0800

    传统多线程

--- src/main/java/org/javacore/thread/BasicThreads.java
+++ src/main/java/org/javacore/thread/BasicThreads.java
@@ -1,25 +1,9 @@
 package org.javacore.thread;
 
-/*
- * Copyright [2015] [Jeff Lee]
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *   http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 /**
- * @author Jeff Lee
- * @since 2015-11-2 17:06:48
- * 	线程简单使用-启动LiftOff线程{@link LiftOff}
+ * 传统多线程的调用是 Thread -> start
+ * Thread构造函数有个Runnable target,target是一个继承了Runnable接口重写了run方法的类
+ * Thread类中调用顺序 start->start0->run->target.run
  */
 public class BasicThreads {
     public static void main(String[] args) {

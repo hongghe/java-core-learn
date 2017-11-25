8474456214b9df7fcc59dcf270b4be45d2e76594
haobin <815754194@qq.com>
2017-11-25 22:41:43 +0800

    修改product-comsutomer

--- src/main/java/org/javacore/thread/ProductAndCustomer.java
+++ src/main/java/org/javacore/thread/ProductAndCustomer.java
@@ -14,6 +14,7 @@ class Depot {
         this.size = 0;
     }
 
+    // 生产
     public synchronized void produce(int val) {
         try {
             // left 表示“想要生产的数量”(有可能生产量太多，需多此生产)
@@ -22,11 +23,14 @@ class Depot {
                 // 库存已满时，等待“消费者”消费产品。
                 while (size >= capacity)
                     wait();
+
                 // 获取“实际生产的数量”(即库存中新增的数量)
                 // 如果“库存”+“想要生产的数量”>“总的容量”，则“实际增量”=“总的容量”-“当前容量”。(此时填满仓库)
                 // 否则“实际增量”=“想要生产的数量”
-                int inc = (size+left)>capacity ? (capacity-size) : left;
+                int inc = (size+left) > capacity ? (capacity-size) : left;
+                // 库存增加
                 size += inc;
+                // 减去已经生产的
                 left -= inc;
                 System.out.printf("%s produce(%3d) --> left=%3d, inc=%3d, size=%3d\n",
                     Thread.currentThread().getName(), val, left, inc, size);
@@ -37,9 +41,10 @@ class Depot {
         }
     }
 
+    // 消费
     public synchronized void consume(int val) {
         try {
-            // left 表示“客户要消费数量”(有可能消费量太大，库存不够，需多此消费)
+            // left 表示“客户要消费数量”(有可能消费量太大，库存不够，需多次消费)
             int left = val;
             while (left > 0) {
                 // 库存为0时，等待“生产者”生产产品。
@@ -49,8 +54,11 @@ class Depot {
                 // 如果“库存”<“客户要消费的数量”，则“实际消费量”=“库存”；
                 // 否则，“实际消费量”=“客户要消费的数量”。
                 int dec = (size<left) ? size : left;
+
+                // 消费
                 size -= dec;
                 left -= dec;
+
                 System.out.printf("%s consume(%3d) <-- left=%3d, dec=%3d, size=%3d\n",
                     Thread.currentThread().getName(), val, left, dec, size);
                 notifyAll();
@@ -106,6 +114,7 @@ public class ProductAndCustomer {
         Producer mPro = new Producer(mDepot);
         Customer mCus = new Customer(mDepot);
 
+        // 三个生产者，两个消费者
         mPro.produce(60);
         mPro.produce(120);
         mCus.consume(90);
